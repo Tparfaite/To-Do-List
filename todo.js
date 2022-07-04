cd9b52bc202c13a372fd9e4c4cb1e3e9164e1180
Tparfaite <parfaitetwagira@gmail.com>
2022-07-04 08:13:11 +0200

    Feat:commit js -Add js and css [Finishes#]

--- todo.js
+++ todo.js
@@ -14,6 +14,8 @@ button.textContent="+";
 button.classList.add("btn");
 let br1=document.createElement("br");
 whole.appendChild(br1);
+let br2=document.createElement("br");
+whole.appendChild(br2);
 
 let text1=document.getElementById("texte");
 
@@ -28,8 +30,34 @@ button.addEventListener("click",function(e){
    down.textContent=text.value;
    whole.appendChild(down);
    text.value="";
+   down.style.backgroundColor="rgb(135,144,144";
+   let br3=document.createElement("br");
+whole.appendChild(br3);
+
+   let done=document.createElement("button");
+   done.setAttribute("type","submit");
+   done.classList.add("done");
+   down.appendChild(done);
+   done.textContent="Done";
+   done.style.color="green";
+
+   let  delet=document.createElement("button");
+   delet.setAttribute("type","submit");
+   delet.classList.add("delete");
+   delet.textContent="Delete";
+   down.appendChild(delet);
+   delet.style.color="red";
 
 
+done.addEventListener("click",function(e){
+    down.style.textDecoration="line-through";
+})
+
+
+delet.addEventListener("click",function(e){
+  confirm(`Do you want to delete ?`);
+    whole.removeChild(down);
+})
 
 
 
